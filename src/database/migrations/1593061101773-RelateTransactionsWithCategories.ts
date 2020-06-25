4063ed91fe2807bedd1c5c83e80b371f5febcc43
Caio Lucas <lienscarlet@gmail.com>
2020-06-25 02:13:41 -0300

    add: transaction, category and relationship with transaction & category

---
+++ src/database/migrations/1593061101773-RelateTransactionsWithCategories.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+class RelateTransactionsWithCategories1593061101773
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'transaction_category',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'transaction_category');
+  }
+}
+
+export default RelateTransactionsWithCategories1593061101773;
